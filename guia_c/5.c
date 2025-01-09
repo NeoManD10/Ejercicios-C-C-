5a3e941d58626baed7c8b06442b6c698ba8c5404
Manuel Dios <mdios@utem.cl>
2025-01-08 21:47:45 -0300

    avance 5.c

--- guia_c/5.c
+++ guia_c/5.c
@@ -1,4 +1,22 @@
 /*5. Escriba un algoritmo que lea números enteros, hasta que se ingrese un número negativo. El             
 Algoritmo debe entregar la suma de aquellos números pares ingresados.                                      
 */                                                                                                         
+
+#include <stdio.h>
+
+int main(){
+    int num;
+
+    do{
+
+        
+
+    }while(num >= 0);
     
+
+
+
+
+
+    return 0;
+}
