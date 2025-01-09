295e5fa082557ac65cb21d273d8ef4977f757e51
Manuel Dios <mdios@utem.cl>
2025-01-08 21:37:55 -0300

    4.c

--- guia_c/4.c
+++ guia_c/4.c
@@ -1,3 +1,31 @@
 /*Escribir un algoritmo que lea 10 números enteros mayores que 100 y entregue la suma del                  
 primer dígito de cada uno de ellos. Ej. Se ingresan 231, 140, 105, 2341, 9876, 315,111,7685,               
 654322,987, el programa debe entregar: 41*/ 
+
+#include <stdio.h>
+
+int main(){
+    int i,num[i],aux=0,res=0,contador;
+
+    for(i = 0; i < 10; i++){
+        printf("Ingresa un numero mayor que 100: "); scanf("%i", &num[i]);
+
+        if (num[i] < 100){
+            printf("Sigue las instrucciones!!\n");
+            return 0;
+        }
+    }
+
+    for (i = 0; i < 10; i++){
+        while (num[i] >= 10){
+            num[i] = num[i] / 10;
+        }   
+
+        res = res + num[i];
+    }
+    
+
+    printf("La suma del primer digito de cada numero es: %i\n", res);
+
+    return 0;
+}
