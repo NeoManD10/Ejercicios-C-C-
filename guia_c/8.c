d4aca2cbf82c149078dd0fac51d78bbe9de400a3
Manuel Dios <mdios@utem.cl>
2025-01-12 03:43:00 -0300

    8.c

--- guia_c/8.c
+++ guia_c/8.c
@@ -1,7 +1,32 @@
 /*Escriba un algoritmo que imprima esta figura:                                                            
-******                                                                                                     
-*****                                                                                                      
-****                                                                                                       
-***                                                                                                        
-**                                                                                                         
-*/
+*****                                                                                                     
+****                                                                                                      
+***                                                                                                       
+**                                                                                                        
+*                                                                                                         
+                                              */
+                                             
+#include<stdio.h>
+
+int main(){
+    int filas,i,j,aux;
+
+    printf("Ingrese una cantidad de filas: "); scanf("%i", &filas);
+
+    aux = filas;
+
+    for (i = 0; i < filas; i++){
+        for (j = 0; j < aux; j++){
+            printf("*");
+        }
+        aux--;
+        printf("\n");
+    }
+    
+
+
+
+
+
+    return 0;
+}
